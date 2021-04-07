9be7ed9fd2be1ef2549d084376c7a904376a0542
pyneng <pyneng.course@gmail.com>
2021-04-07 06:09:40 +0000

    jianjs examples

--- examples/20_jinja2/basic_generator.py
+++ examples/20_jinja2/basic_generator.py
@@ -1,27 +1,13 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+import yaml
 
-template = Template(
-    """
-hostname {{name}}
-!
-interface Loopback255
- description Management loopback
- ip address 10.255.{{id}}.1 255.255.255.255
-!
-interface GigabitEthernet0/0
- description LAN to {{name}} sw1 {{int}}
- ip address {{ip}} 255.255.255.0
-!
-router ospf 10
- router-id 10.255.{{id}}.1
- auto-cost reference-bandwidth 10000
- network 10.0.0.0 0.255.255.255 area 0
-"""
-)
+
+env = Environment(loader=FileSystemLoader("."))
+templ = env.get_template("cfg_template.txt")
 
 liverpool = {"id": "11", "name": "Liverpool", "int": "Gi1/0/17", "ip": "10.1.1.10"}
+print(templ.render(liverpool))
 
-print(template.render(liverpool))
 """
 Examples:
 
