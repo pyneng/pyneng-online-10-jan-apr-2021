82c5641709a620101e4dd5e02370952454b7339e
pyneng <pyneng.course@gmail.com>
2021-04-10 10:56:02 +0000

    lecture 21 examples

--- examples/22_oop_basics/class_network.py
+++ examples/22_oop_basics/class_network.py
@@ -2,22 +2,36 @@ import ipaddress
 
 
 class Network:
-    def __init__(self, network):
+    all_allocated_ip = []
+
+    def __init__(self, network, mask):
         self.network = network
-        address, mask = network.split("/")
-        self.network_address = address
-        self.mask = int(mask)
-        self.bin_mask = "1" * self.mask + "0" * (32 - self.mask)
+        self.mask = mask
+        self.allocated = []
+        ipv4net = ipaddress.ip_network(f"{self.network}/{self.mask}")
+        self.hosts = [str(ip) for ip in ipv4net.hosts()]
 
-    def hosts(self):
-        net = ipaddress.ip_network(self.network)
-        return [str(ip) for ip in net.hosts()]
-
-    def __repr__(self):
-        return f"Network('{self.network}')"
+    def allocate_ip(self, ip):
+        if ip in self.hosts:
+            if ip not in self.allocated:
+                self.allocated.append(ip)
+                # Network.all_allocated_ip.append(ip)
+                type(self).all_allocated_ip.append(ip)
+            else:
+                raise ValueError(
+                    f"IP-адрес {ip} уже находится в allocated "
+                    f"\n{self.allocated}"
+            )
+        else:
+            raise ValueError(
+                f"IP-адрес {ip} не входит в сеть {self.network}/{self.mask}"
+            )
 
     def __str__(self):
-        return f"{self.network}"
+        return f"{self.network}/{self.mask}"
+
+    def __repr__(self):
+        return f"Network('{self.network}', {self.mask})"
 
     def __len__(self):
-        return len(self.hosts())
+        return len(self.hosts)
